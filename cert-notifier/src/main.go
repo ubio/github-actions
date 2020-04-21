df96346a010a79826e6c83f70ed2cc7e815f9ca3
Andrew Waters <waters@me.com>
2020-04-21 22:51:25 +0100

    Add debug

--- cert-notifier/src/main.go
+++ cert-notifier/src/main.go
@@ -49,7 +49,7 @@ func main() {
 	expiring := make([]cert, 0)
 	for _, cert := range certs {
 		expires := cert.until()
-		fmt.Printf("Checked %s - expires in %d days\n", cert.DomainName, expires)
+		fmt.Printf("Checked %s - expires in %d days (%s)\n", cert.DomainName, expires, cert.NotAfter)
 		if expires < warnUnderDays {
 			expiring = append(expiring, cert)
 		}
