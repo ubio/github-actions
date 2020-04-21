161adf0ebd6f6ad6f6d8ffe8868b491dbfa47878
Andrew Waters <waters@me.com>
2020-04-21 22:06:52 +0100

    ADds input unmarshalling

---
+++ cert-notifier/go.mod
@@ -0,0 +1,5 @@
+module github.com/universalbasket/github-actions/cert-notifier
+
+go 1.13
+
+require github.com/prometheus/common v0.9.1 // indirect
--- cert-notifier/src/main.go
+++ cert-notifier/src/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"os"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/prometheus/common/log"
 )
 
+type cert struct {
+	Domain     string   `json:"domainName"`
+	IP         string   `json:"ip"`
+	Issuer     string   `json:"issuer"`
+	CommonName string   `json:"commonName"`
+	NotBefore  string   `json:"notBefore"`
+	NotAfter   string   `json:"notAfter"`
+	Error      string   `json:"error"`
+	Sans       []string `json:"sans"`
+}
+
 func main() {
-	for _, pair := range os.Environ() {
-		fmt.Println(pair)
+	input := os.Getenv("INPUT_CERTS")
+
+	certs := make([]cert, 0)
+	if err := json.Unmarshal([]byte(input), &certs); err != nil {
+		log.Fatal(err)
 	}
+	spew.Dump(certs)
 }
