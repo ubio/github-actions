e0a1eb37763819d774492cfde9824ea246752f00
Andrew Waters <waters@me.com>
2020-04-22 09:56:15 +0100

    Update attachment

--- cert-notifier/src/main.go
+++ cert-notifier/src/main.go
@@ -74,16 +74,9 @@ func warn(certs []cert) {
 	attachments := make([]slack.Attachment, 0)
 
 	attachments = append(attachments, slack.Attachment{
-		Color:         "good",
-		Fallback:      "You successfully posted by Incoming Webhook URL!",
-		AuthorName:    "slack-go/slack",
-		AuthorSubname: "github.com",
-		AuthorLink:    "https://github.com/slack-go/slack",
-		AuthorIcon:    "https://avatars2.githubusercontent.com/u/652790",
-		Text:          "<!channel> All text in Slack uses the same system of escaping: chat messages, direct messages, file comments, etc. :smile:\nSee <https://api.slack.com/docs/message-formatting#linking_to_channels_and_users>",
-		Footer:        "slack api",
-		FooterIcon:    "https://platform.slack-edge.com/img/default_application_icon.png",
-		Ts:            json.Number(strconv.FormatInt(time.Now().Unix(), 10)),
+		Color:    "good",
+		Fallback: "You successfully posted by Incoming Webhook URL!",
+		Text:     "Text in Slack uses the same system of escaping: chat messages, direct messages, file comments, etc. :smile:\nSee <https://api.slack.com/docs/message-formatting#linking_to_channels_and_users>",
 	})
 	msg := slack.WebhookMessage{
 		Attachments: attachments,
