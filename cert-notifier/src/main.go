1119e13c04a9345e14a216ba0c0029e12f7de3a4
Andrew Waters <waters@me.com>
2020-04-22 11:41:53 +0100

    Add NL

--- cert-notifier/src/main.go
+++ cert-notifier/src/main.go
@@ -26,10 +26,10 @@ func (c cert) slackBlock() *slack.SectionBlock {
 	return slack.NewSectionBlock(
 		nil,
 		[]*slack.TextBlockObject{
-			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":globe_with_meridians: %s", c.DomainName), false, false),
-			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":point_right: *IP:* %s", c.IP), false, false),
-			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*:suspect: Issuer:* %s", c.Issuer), false, false),
-			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*:boom: Expires in:* (%d days)", c.until()), false, false),
+			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":globe_with_meridians: %s\n", c.DomainName), false, false),
+			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":point_right: %s\n", c.IP), false, false),
+			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":suspect: %s\n", c.Issuer), false, false),
+			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":boom: %d days\n", c.until()), false, false),
 		},
 		nil,
 	)
