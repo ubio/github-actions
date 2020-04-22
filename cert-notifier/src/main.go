0c039b16b97f6e16ad2b6c498cd575db7d60f0b6
Andrew Waters <waters@me.com>
2020-04-22 10:21:57 +0100

    Test message blocks

--- cert-notifier/src/main.go
+++ cert-notifier/src/main.go
@@ -67,26 +67,68 @@ func warn(certs []cert) {
 	fmt.Println("-----------------")
 	fmt.Println("!!! WARNINGS: !!!")
 	fmt.Println("")
+
 	for _, cert := range certs {
 		fmt.Println(cert.DomainName, "expiring in", cert.until(), "days")
 	}
+	headerText := slack.NewTextBlockObject("mrkdwn", "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*", false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	// Fields
+	typeField := slack.NewTextBlockObject("mrkdwn", "*Type:*\nComputer (laptop)", false, false)
+	whenField := slack.NewTextBlockObject("mrkdwn", "*When:*\nSubmitted Aut 10", false, false)
+	lastUpdateField := slack.NewTextBlockObject("mrkdwn", "*Last Update:*\nMar 10, 2015 (3 years, 5 months)", false, false)
+	reasonField := slack.NewTextBlockObject("mrkdwn", "*Reason:*\nAll vowel keys aren't working.", false, false)
+	specsField := slack.NewTextBlockObject("mrkdwn", "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\"", false, false)
+
+	fieldSlice := make([]*slack.TextBlockObject, 0)
+	fieldSlice = append(fieldSlice, typeField)
+	fieldSlice = append(fieldSlice, whenField)
+	fieldSlice = append(fieldSlice, lastUpdateField)
+	fieldSlice = append(fieldSlice, reasonField)
+	fieldSlice = append(fieldSlice, specsField)
+
+	fieldsSection := slack.NewSectionBlock(nil, fieldSlice, nil)
+
+	// Approve and Deny Buttons
+	approveBtnTxt := slack.NewTextBlockObject("plain_text", "Approve", false, false)
+	approveBtn := slack.NewButtonBlockElement("", "click_me_123", approveBtnTxt)
+
+	denyBtnTxt := slack.NewTextBlockObject("plain_text", "Deny", false, false)
+	denyBtn := slack.NewButtonBlockElement("", "click_me_123", denyBtnTxt)
+
+	actionBlock := slack.NewActionBlock("", approveBtn, denyBtn)
+
+	// Build Message with blocks created above
+
+	msg := slack.NewBlockMessage(
+		headerSection,
+		fieldsSection,
+		actionBlock,
+	)
 
 	api := slack.New(os.Getenv("INPUT_SLACK_TOKEN"))
-	attachment := slack.Attachment{
-		Pretext: "some pretext",
-		Text:    "some text",
-		Fields: []slack.AttachmentField{
-			slack.AttachmentField{
-				Title: "a",
-				Value: "no",
-			},
-		},
-	}
-
-	channelID, timestamp, err := api.PostMessage("@aw", slack.MsgOptionText("Some text", false), slack.MsgOptionAttachments(attachment))
+	_, _, err := api.PostMessage("@aw", slack.MsgOptionText(msg, true))
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		log.Fatal(err)
 		return
 	}
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+
+	// attachment := slack.Attachment{
+	// 	Pretext: "some pretext",
+	// 	Text:    "some text",
+	// 	Fields: []slack.AttachmentField{
+	// 		slack.AttachmentField{
+	// 			Title: "a",
+	// 			Value: "no",
+	// 		},
+	// 	},
+	// }
+
+	// channelID, timestamp, err := api.PostMessage("@aw", slack.MsgOptionText("Some text", false), slack.MsgOptionAttachments(attachment))
+	// if err != nil {
+	// 	fmt.Printf("%s\n", err)
+	// 	return
+	// }
+	// fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 }
