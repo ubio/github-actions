516183297d1859c3e0a4d145f189bd88bac21a21
Anthony Sterling <as@ub.io>
2020-07-09 13:26:39 +0100

    add some error handling to looking up env vars

--- rsslack/src/main.go
+++ rsslack/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,11 +10,37 @@ import (
 
 func main() {
 
-	api := slack.New(os.Getenv("INPUT_SLACK_TOKEN"))
-	channelID, timestamp, err := api.PostMessage(os.Getenv("INPUT_CHANNEL"), slack.MsgOptionText(os.Getenv("INPUT_MESSAGE"), false))
+	token, err := env("INPUT_SLACK_TOKEN")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	channel, err := env("INPUT_CHANNEL")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message, err := env("INPUT_MESSAGE")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api := slack.New(token)
+	channelID, timestamp, err := api.PostMessage(channel, slack.MsgOptionText(message, false))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Printf("Message sent to channel %s at %s", channelID, timestamp)
+}
+
+func env(key string) (string, error) {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return "", fmt.Errorf("env key %s does not exist", key)
+	}
+	if len(value) == 0 {
+		return "", fmt.Errorf("env key %s is empty", key)
+	}
+	return value, nil
 }
