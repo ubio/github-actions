d4e8d42e8d89f2a574ec73fe610372e9c5d8cb93
Andrew Waters <andrew-waters@users.noreply.github.com>
2021-09-18 16:00:10 +0100

    Enable context

--- slack-discussion/src/main.go
+++ slack-discussion/src/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	"github.com/eritikass/githubmarkdownconvertergo"
 
@@ -90,37 +92,38 @@ func buildSlackBlock(d *Discussion) slack.MsgOption {
 	dividerSection := slack.NewDividerBlock()
 
 	// @TODO: get the squad name from the event
-	// squadName := "Proxies Squad"
+	squadName := "Proxies Squad"
 
 	// viewDiscussionText := "View Discussion"
 
-	// date, err := time.Parse(
-	// 	time.RFC3339,
-	// 	d.CreatedAt,
-	// )
-	// if err != nil {
-	// 	log.Fatal("error with date parsing:", err)
-	// }
+	date, err := time.Parse(
+		time.RFC3339,
+		d.CreatedAt,
+	)
+	if err != nil {
+		log.Fatal("error with date parsing:", err)
+	}
 
-	// day := date.Format("Mon, Jan 2")
-	// tod := date.Format("03:04")
+	day := date.Format("Mon, Jan 2")
+	tod := date.Format("03:04")
 
-	// contextHeaderText := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("%s %s - *%s*\n%s @ %s by <%s|%s>", d.Category.Emoji, squadName, d.Category.Name, day, tod, d.User.HTMLURL, d.User.Login), false, false)
+	contextHeaderText := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("%s %s - *%s*\n%s @ %s by <%s|%s>", d.Category.Emoji, squadName, d.Category.Name, day, tod, d.User.HTMLURL, d.User.Login), false, false)
 
 	// buttonText := slack.NewTextBlockObject("plaintext", viewDiscussionText, false, false)
 	// button := slack.NewButtonBlockElement(fmt.Sprintf("discussion-link-%d", d.ID), d.HTMLURL, buttonText)
 	// linkAccessory := slack.NewAccessory(button)
 
 	// contextSection := slack.NewSectionBlock(contextHeaderText, nil, linkAccessory)
+	contextSection := slack.NewSectionBlock(contextHeaderText, nil, nil)
 
 	headerText := slack.NewTextBlockObject("mrkdwn", d.Title, false, false)
 	headerSection := slack.NewSectionBlock(headerText, nil, nil)
 
 	bodyText := slack.NewTextBlockObject("mrkdwn", githubmarkdownconvertergo.Slack(d.Body), false, false)
-	bodySection := slack.NewSectionBlock(bodyText, nil, nil)
+	bodySection := slack.NewHeaderBlock(bodyText, nil, nil)
 
 	return slack.MsgOptionBlocks(
-		// contextSection,
+		contextSection,
 		dividerSection,
 		headerSection,
 		bodySection,
